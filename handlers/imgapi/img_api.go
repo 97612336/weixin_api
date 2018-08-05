fbdff4b7cacf30e4c8dd2600de6fb9f86cd639fe
家虚拟机 <xkjskk@163.com>
2018-08-05 12:34:26 +0800

    更改为服务器版

--- handlers/imgapi/img_api.go
+++ handlers/imgapi/img_api.go
@@ -3,7 +3,6 @@ package imgapi
 import (
 	"net/http"
 	"weixin_api/util"
-	"fmt"
 	"log"
 	"weixin_api/config"
 	"os"
@@ -12,15 +11,14 @@ import (
 
 func Receive_img(w http.ResponseWriter, r *http.Request) {
 	//解析表单
-	r.ParseForm()
+	//r.ParseForm()
+	r.ParseMultipartForm(1024 * 1024 * 3)
 	//判断方法
 	if r.Method == "POST" {
 		username := r.PostFormValue("username")
 		password := r.PostFormValue("password")
 		account := util.Get_img_account()
 		if account.Upload_name == username && account.Upload_password == password {
-			//操作保存
-			fmt.Print("执行到这里了？")
 			//获取表单文件
 			file, header, err := r.FormFile("img")
 			if err != nil {
@@ -30,18 +28,20 @@ func Receive_img(w http.ResponseWriter, r *http.Request) {
 			}
 			defer file.Close()
 			//创建写入到本地的文件
-			file_path_and_name := config.Static_Path + header.Filename
+			//file_path_and_name := config.Static_Path + header.Filename
+			file_path_and_name := config.Static_path_web + header.Filename
 			f, err := os.Create(file_path_and_name)
 			if err != nil {
 				log.Println("创建本地文件的时候出错")
 				log.Println(err)
 			}
 			defer f.Close()
-			url := config.Local_href + file_path_and_name
+			//url := config.Local_href + header.Filename
+			url := config.Static_href + header.Filename
 			//执行写入操作
 			io.Copy(f, file)
 			json_str := "{ \"code\":200," +
-				"\"url\":" + url +
+				"\"url\":\"" + url + "\"" +
 				"}"
 			util.Return_json(w, json_str)
 			return
